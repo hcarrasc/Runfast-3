7c99c3a89e415f3586da4ad16b09d5c65caa047a
Hector <HC-d4!@MBP-de-HC.local>
2023-08-20 04:47:15 -0400

    adding electron modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import Audic from 'audic';
+/*
+import Audic from "audic";
 
-const path = '/Users/hc/Music/Music\ HC/';
+const path = "/Users/hc/Music/Music HC/";
 
-const audic = new Audic(path + 'Franz Hlusek _ Jorge Nava - Cosmic Dreamer (Fast Distance Mix).mp3');
+const audic = new Audic(
+  path + "Franz Hlusek _ Jorge Nava - Cosmic Dreamer (Fast Distance Mix).mp3"
+);
 await audic.play();
 
-audic.addEventListener('ended', () => {
-	audic.destroy();
+audic.addEventListener("ended", () => {
+  audic.destroy();
+});
+*/
+
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("src/index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
